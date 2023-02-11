0b5c5d5e3e94d6e47571aacf15b7f4b4432a6176
DESKTOP-KIP9E0Q\jason <jasonwaidog@hotmial.com>
2023-02-11 21:48:53 +0800

    init

---
+++ 2023-02 IncidentsAnalization/2023-02 IncidentsAnalization/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace _2023_02_IncidentsAnalization.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
